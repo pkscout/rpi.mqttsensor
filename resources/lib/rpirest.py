79da80497b17c54c30b383402b163f3cba4f03f6
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2022-01-06 19:24:42 -0500

    fixes to get data into HA sensors properly

--- resources/lib/rpirest.py
+++ resources/lib/rpirest.py
@@ -19,13 +19,14 @@ class PassSensorData:
         self.READINGDELTA = config.Get('readingdelta') * 60
         self.WHICHSENSOR = config.Get('which_sensor')
         self.SENSOR = self._pick_sensor()
+        self.TEMPSCALE = '°C'
         headers = {}
         headers['Content-Type'] = 'application/json'
         headers['Accept'] = 'application/json'
         headers['Authorization'] = 'Bearer %s' % config.Get('rest_token')
         self.JSONURL = url.URL('json', headers=headers)
-        self.RESTURL = 'http://%s:%s/api/states/%s' % (config.Get(
-            'rest_address'), config.Get('rest_port'), config.Get('rest_state'))
+        self.RESTURL = 'http://%s:%s/api/states/sensor.%s' % (config.Get(
+            'rest_address'), config.Get('rest_port'), config.Get('rest_state_name'))
 
     def Start(self):
         self.LW.log(['starting up PassSensorData'], 'info')
@@ -35,14 +36,25 @@ class PassSensorData:
             while self.KEEPRUNNING:
                 temperature = self._convert_temp(self.SENSOR.Temperature())
                 humidity = self.SENSOR.Humidity()
-                if temperature != stored_temp or humidity != stored_humidity:
-                    stored_temp = temperature
-                    stored_humidity = humidity
-                    payload = {'state': 'data', 'attributes': {
-                        'temperature': temperature, 'humidity': humidity}}
+                if int(temperature) != stored_temp:
+                    stored_temp = int(temperature)
+                    payload = {'state': temperature, "attributes": {
+                        "unit_of_measurement": self.TEMPSCALE, "friendly_name": "RPI Sensor Temperature"}}
                     status, loglines, results = self.JSONURL.Post(
-                        self.RESTURL, data=json.dumps(payload))
+                        self.RESTURL + 'temp', data=json.dumps(payload))
                     self.LW.log(loglines)
+                else:
+                    self.LW.log(['no change to temperature'])
+                if int(humidity) != stored_humidity:
+                    stored_humidity = int(humidity)
+                    payload = {'state': humidity, "attributes": {
+                        "unit_of_measurement": "%", "friendly_name": "RPI Sensor Humidity"}}
+                    status, loglines, results = self.JSONURL.Post(
+                        self.RESTURL + 'humidity', data=json.dumps(payload))
+                    self.LW.log(loglines)
+                else:
+                    self.LW.log(['no change to humidity'])
+
                 time.sleep(self.READINGDELTA)
         except KeyboardInterrupt:
             self.KEEPRUNNING = False
@@ -53,6 +65,7 @@ class PassSensorData:
 
     def _convert_temp(self, temperature):
         if config.Get('temp_scale').lower() == 'f':
+            self.TEMPSCALE = '°F'
             return (temperature * 1.8) + 32
         return temperature
 
